b696cab869e725a01030c98035576d7db12a1368
Quan Tran <quan@shousio.com>
2021-07-19 10:38:30 -0500

    Update documentation and add support for LLVM 10 and later (#1)
    * Update to LLVM 12.0.0
    * Fix unused variable warnings
    * Fix multiple lc_disabled_count definitions and broken CI config calls
    * Fix unused variable warning
    * Update to LLVM 12.0.0
    * Fix code formatting for the API and demo
    * Remove duplicate makefile in raytrace
    * Fix unused function attribute on gcc
    * Update makefile to be compatible with macOS
    * Update example to be more concise
    * Fix code formatting for the API and pass using LLVM style
    * Update support for both LLVM 9 and LLVM 12
    * Fix typo and code formatting for the pass
    * Update register pass usage from `logicalclock` to `compilerinterrupt`
    * Revert "Update register pass usage from `logicalclock` to `compilerinterrupt`"
    This reverts commit 3466a25f706d3dd89684030747e77847c21ef393.
    * Update Compiler Interrupts API
    * Fix some typos and formatting in the pass
    * Update Makefile
    * Update README.md
    * Fix thread local static declaration on gcc
    * Update Makefile
    * Update example/Makefile to silence build commands by default
    * Add support for LLVM 10 and 11
    * Update README.md
    
    Co-authored-by: Nilanjana Basu <nilanjana.basu87@gmail.com>

--- src/ci_lib.h
+++ src/ci_lib.h
@@ -1,11 +1,12 @@
-#include <unistd.h>
 #include <stdint.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 /* interrupt handler prototype */
 typedef void (*ci_handler)(long);
 
-/* prototype of function that can be called before CI is disabled or after CI is enabled in the interrupt handler */
+/* prototype of function that can be called before CI is disabled or
+ * after CI is enabled in the interrupt handler */
 typedef void (*ci_margin_hook)(void);
 
 #ifdef __cplusplus
@@ -16,38 +17,38 @@ extern "C" {
 extern __thread int LocalLC;
 extern __thread int lc_disabled_count;
 
-
-/****************************************** 
- *        CI configuration API            *
+/******************************************
+ *          CI configuration API          *
  * All configurations are thread-specific *
  ******************************************/
 
 /* register interrupt handler */
-int register_ci(int, int, ci_handler);
+void register_ci(int, int, ci_handler);
 
 /* de-register interrupt handler */
-void deregister();
+void deregister_ci(void);
 
-/* register a function to be called just before CI is disabled in the interrupt handler */
+/* register a function to be called just before
+ * CI is disabled in the interrupt handler */
 void register_ci_disable_hook(ci_margin_hook ci_func);
 
-/* register a function to be called just after CI is enabled in the interrupt handler */
+/* register a function to be called just after
+ * CI is enabled in the interrupt handler */
 void register_ci_enable_hook(ci_margin_hook ci_func);
 
 /* disable interrupt calls */
-void ci_disable();
+void ci_disable(void);
 
 /* enable interrupt calls */
-void ci_enable();
+void ci_enable(void);
 
-/* disable probe instrumentation, that is, the code should be non-preemptible*/
-void instr_disable();
+/* disable probe instrumentation, code should be non-preemptible */
+void instr_disable(void);
 
 /* enable probe instrumentation */
-void instr_enable();
+void instr_enable(void);
 
-
-/* for internal use by LIBCI & CI Pass */
+/* for internal use by CI API and CI Pass */
 
 /* CI pass interrupt handler */
 extern __thread ci_handler intvActionHook;
@@ -58,10 +59,11 @@ extern __thread uint64_t ci_ir_interval;
 /* CI (cycles) pass IR reset interval when the target cycles is not exceeded */
 extern __thread uint64_t ci_reset_ir_interval;
 
-/* CI (cycles) pass Cycles interrupt interval */
+/* CI (cycles) pass cycles interrupt interval */
 extern __thread uint64_t ci_cycles_interval;
 
-/* CI (cycles) pass Cycles interrupt threshold to fire the interrupt or reset the IR counter */
+/* CI (cycles) pass cycles interrupt threshold to fire the interrupt or
+ * reset the IR counter */
 extern __thread uint64_t ci_cycles_threshold;
 
 #ifdef __cplusplus
